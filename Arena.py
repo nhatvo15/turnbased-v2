d99b9652e2aea06d12fc2024bf6e30bc71684491
Nhat Vo <nvo001@student.cs.plattsburgh.edu>
2016-04-26 10:40:11 -0400

    v1 - barebone

---
+++ Arena.py
@@ -0,0 +1,47 @@
+from Player import *
+from World import *
+
+########################
+#                      #
+#        Menus         #
+#                      #
+########################
+"""
+Shop: handle transaction
+restockable
+"""
+def shop():
+    pass
+
+"""
+Stat: information of the player
+toggle-able
+"""
+def stat():
+    pass
+
+"""
+Inventory: carry item, equiped items, gold
+toggle-able
+"""
+def inventory():
+    pass
+
+"""
+Play:
+
+have: size, coordinate, obstackle, bots, boss, npc, secret shop
+does: traverse, fight bots, fights boss
+loot: exp, gold, dropped-items, secret key
+"""
+def play():
+    pass
+
+
+
+def main():
+    player = Player()
+    world = World()
+    
+main()
+    
